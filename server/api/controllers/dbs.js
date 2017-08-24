e8884e1f9bada2efdc48047c5bbe7726be390e2a
ilayhoshen <ilayhoshen@gmail.com>
2017-08-24 23:16:58 +0300

    connection one time

--- server/api/controllers/dbs.js
+++ server/api/controllers/dbs.js
@@ -2,13 +2,16 @@ var MongoClient = require('mongodb').MongoClient;
 let Q = require('q');
 var url = 'mongodb://carebnb:Password1@ds157833.mlab.com:57833/carebnb';
 
+let conn  = undefined;
 function getConnection() {
     return new Promise((resolve, reject) => {
+        if(conn) return resolve(conn);
         MongoClient.connect(url, function (err, db) {
             if (err) {
                 return console.error(err)
             }
             console.log("Connected correctly to server");
+            conn = db;
             resolve(db);
         });
     });
