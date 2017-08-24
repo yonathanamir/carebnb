2fce25032968c246686fcc24b1e1d3b99d312d5c
Yonathan <tunamir18@gmail.com>
2017-08-24 16:27:49 +0300

    material design ftw

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+    'ngMaterial',
   'myApp.view1',
   'myApp.view2',
+  'myApp.resource-submission',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
